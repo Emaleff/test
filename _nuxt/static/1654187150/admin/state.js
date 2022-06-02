d82a68cd95824e02c9ba1e96ade4b6ffc89c2b33
emaleff <emalev91@gmail.com>
2022-06-02 23:30:26 +0700

    Updates

--- _nuxt/static/1654187150/admin/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftest\u002F_nuxt\u002Fstatic\u002F1654187150",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fadmin",config:{_app:{basePath:"\u002Ftest\u002F",assetsPath:"\u002Ftest\u002F_nuxt\u002F",cdnURL:a}}}}(null));
